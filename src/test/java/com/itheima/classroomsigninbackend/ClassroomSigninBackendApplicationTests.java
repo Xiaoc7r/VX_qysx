fa068f6af2d530d27629975acd96da01b1b4d904
Xiaoc7r <xxiaocr@gmail.com>
2026-03-09 10:03:42 +0800

    初始配置
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/itheima/classroomsigninbackend/ClassroomSigninBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.classroomsigninbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomSigninBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
